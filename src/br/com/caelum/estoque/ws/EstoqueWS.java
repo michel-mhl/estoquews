05acf4ea952bdaef26f2a232999fff1a327249a0
michel_mhl <michel_mhl@hotmail.com>
2022-05-06 11:29:06 -0300

    criando projeto aws

---
+++ src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -0,0 +1,15 @@
+package br.com.caelum.estoque.ws;
+
+import br.com.caelum.estoque.modelo.item.ItemDao;
+
+public class EstoqueWS {
+	
+	 private ItemDao dao = new ItemDao();
+	 
+	 public List
+
+
+	 
+	 
+
+}
